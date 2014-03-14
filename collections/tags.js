a9f684eac47dd606c9ef027ae79896383be0c178
Duane Johnson <duane@instructure.com>
2014-03-14 13:26:16 -0600

    Layout fixes and client-side tags permission fix

--- collections/tags.js
+++ collections/tags.js
@@ -3,13 +3,18 @@ Tags = new Meteor.Collection('tags');
 var upsertTagsForPost = function(userId, doc) {
   var tagsByName = postTagsByName(doc);
   var tags = _.keys(tagsByName);
-
-  _.each(tags, function(tag) {
-    var tagDoc = {name: tag, category: 'audience'};
-    var id = Tags.upsert(tagDoc, tagDoc);
-    // Meteor._debug('upserted tag', id, tagDoc);
-  });
+  Meteor.call('addTags', tags, 'audience');
 };
 
 Posts.before.insert(upsertTagsForPost);
 Posts.before.update(upsertTagsForPost);
+
+Meteor.methods({
+  addTags: function(tags, category) {
+    _.each(tags, function(tag) {
+      var tagDoc = {name: tag, category: category};
+      var id = Tags.upsert(tagDoc, tagDoc);
+      // Meteor._debug('upserted tag', id, tagDoc);
+    });    
+  }
+})
